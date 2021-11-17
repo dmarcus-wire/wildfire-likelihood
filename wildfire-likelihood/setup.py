728083b8c4c1be8ec97a05b490af02d6e90b788b
dmarcus-wire <dmarcus@redhat.com>
2021-11-16 18:08:53 -0700

    initial

---
+++ wildfire-likelihood/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predict the likelihood of a wildfire in the US ',
+    author='wildfire-likelihood',
+    license='MIT',
+)
